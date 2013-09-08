f58ae98b8b92ecf78d195f9270144047a4a32efe
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2013-09-08 14:03:26 +0100

    Code cleanup

--- src/Cli.php
+++ src/Cli.php
@@ -2,7 +2,8 @@
 
 class Cli {
 
-	public function diff($oldRevision, $newRevision) {
+	public function diff($oldRevision, $newRevision) 
+	{
 		$cmd = "git diff --name-status " . $oldRevision . " " . $newRevision;
 		$cmdOutput = shell_exec($cmd);
 	}
