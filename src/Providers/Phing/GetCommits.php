2951f577805015f17cf38386055037e379def6ff
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2013-09-13 23:43:32 +0100

    Corrected autoload paths for correct installation location

--- src/Providers/Phing/GetCommits.php
+++ src/Providers/Phing/GetCommits.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'phing/Task.php';
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../../autoload.php';
 
 use \KevBaldwyn\GitDeploy\Cli;
 
